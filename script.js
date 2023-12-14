e77bc9c93e595abc1391c761155d046f3da34192
Shameer Rose <sham@Shameers-MacBook-Pro.local>
2023-12-14 15:03:22 -0500

    Comment out scripts until index and style are done

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const api_key = "07f0025f90d64b689716046f773c95ba"
+// const api_key = "07f0025f90d64b689716046f773c95ba"
 
-let State = "NY"
+// let State = "NY"
 
-let latitude
-let longitude
+// let latitude
+// let longitude
 
-let convertKelvinToFarenehit = (kelvin) => (kelvin - 273.15) * 1.8 + 32;
+// let convertKelvinToFarenehit = (kelvin) => (kelvin - 273.15) * 1.8 + 32;
 
-const geocode_api_url = `http://api.openweathermap.org/geo/1.0/direct?q=${State}&limit=5&appid=${api_key}`
+// const geocode_api_url = `http://api.openweathermap.org/geo/1.0/direct?q=${State}&limit=5&appid=${api_key}`
 
-let GeoCode = fetch(geocode_api_url)
-    .then(response => response.json())
-    .then(data => {
-        latitude = data[4].lat;
-        longitude = data[4].lon;
-    })
+// let GeoCode = fetch(geocode_api_url)
+//     .then(response => response.json())
+//     .then(data => {
+//         latitude = data[4].lat;
+//         longitude = data[4].lon;
+//     })
     
-GeoCode.then(data => {
-    const current_weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
+// GeoCode.then(data => {
+//     const current_weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
 
-    fetch(current_weather_url)
-        .then(response => response.json())
-        .then(data => {
-            let Temperature = Math.round(convertKelvinToFarenehit(data.main.temp))
-            console.log(`It is ${Temperature} degrees in ${State}`)
-    })})
+//     fetch(current_weather_url)
+//         .then(response => response.json())
+//         .then(data => {
+//             let Temperature = Math.round(convertKelvinToFarenehit(data.main.temp))
+//             weatherHeader.innerHTML = `It is ${Temperature} degrees in ${State}`
+//     })})
