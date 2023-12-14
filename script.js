fc1bbdece0a0ffd54e5b6cb113f769ec14ba32e0
Shameer Rose <sham@Shameers-MacBook-Pro.local>
2023-12-14 17:17:53 -0500

    Update html with weather information

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-// const api_key = "07f0025f90d64b689716046f773c95ba"
+const api_key = "07f0025f90d64b689716046f773c95ba"
 
-// let zipCode = "11701"
+const submitButton = document.getElementById("submit-button")
+let weatherHeader = document.getElementById("weather-header")
+let weatherInput = document.getElementById("weather-input")
 
-// let latitude
-// let longitude
+let latitude
+let longitude
 
-// let areaName
+let areaName
 
-// let convertKelvinToFarenehit = (kelvin) => (kelvin - 273.15) * 1.8 + 32;
+let convertKelvinToFarenehit = (kelvin) => (kelvin - 273.15) * 1.8 + 32;
 
-// const geocode_api_url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${api_key}`
+function GetData(zipCode) {
 
-// let GeoCode = fetch(geocode_api_url)
-//     .then(response => response.json())
-//     .then(data => {
-//         latitude = data.lat;
-//         longitude = data.lon;
-//         areaName = data.name;
-//     })
-    
-// GeoCode.then(data => {
-//     const current_weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
+    const geocode_api_url = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${api_key}`
 
-//     fetch(current_weather_url)
-//         .then(response => response.json())
-//         .then(data => {
-//             let Temperature = Math.round(convertKelvinToFarenehit(data.main.temp))
-//             console.log(`It is ${Temperature} degrees in ${areaName}`)
-//     })})
+    let GeoCode = fetch(geocode_api_url)
+        .then(response => response.json())
+        .then(data => {
+            latitude = data.lat;
+            longitude = data.lon;
+            areaName = data.name;
+      })
+        
+    GeoCode.then(data => {
+        const current_weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}`
+
+        fetch(current_weather_url)
+            .then(response => response.json())
+            .then(data => {
+                let Temperature = Math.round(convertKelvinToFarenehit(data.main.temp))
+                weatherHeader.innerHTML = `It is ${Temperature} degrees in ${areaName}`
+    })})
+}
+
+submitButton.addEventListener("click", () => {
+    if (weatherInput.value != "") {
+        GetData(weatherInput.value)
+    }
+})
